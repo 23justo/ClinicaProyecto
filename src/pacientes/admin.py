7165db7c601636c721b661f27e1817a17afa693f
23justo <justojul23@hotmail.com>
2017-01-16 10:56:00 -0600

    modelos para app pacientes creados migraciones echas sin errores

--- src/pacientes/admin.py
+++ src/pacientes/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from .models import paciente,familia
 # Register your models here.
+class pacienteAdmin(admin.ModelAdmin):
+    list_display = [
+    'nombre',
+    'fechaNacimiento',
+    'telefono',
+    'genero',
+    'edad',
+    'tipoSangre',
+    'dpi',
+    'cui',
+    'idFamilia',
+    ]
+
+class familiaAdmin(admin.ModelAdmin):
+    list_display = [
+    'primerApellido',
+    'segundoApellido',
+    ]
+
+admin.site.register(paciente,pacienteAdmin)
+admin.site.register(familia,familiaAdmin)
